6e413281478ab74b79ce85926b4f6c51c8498a8c
Aleks <lesha.rudiko@gmail.com>
2023-09-27 13:20:17 +0300

    update

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ const btn = document.querySelector('.watch_result')
 const priceInput = document.querySelector('.price_user') 
 const select = document.getElementById('select')
 
-const getDataServe = () => {
-    return fetch('cardb.json')
+const render = (data) => {
+
+    console.log(data);
+//     const arr = data
+
+//    for(let carVariant of arr){
+//     console.log(carVariant.price);
+//    }
+}
+
+const getDataServe = (modelCar) => {
+    return fetch(`./db/${modelCar}.json`)
+        .then((r => r.json()))
 }
 
 const getDataInput = () => {
    if(select.value === '' || priceInput.value === ''){
     alert('Заполните поля')
    } else{
-    getDataServe()
+    getDataServe(select.value)
+        .then((data) => render(data))
    }
 }
 
